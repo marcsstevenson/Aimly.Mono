d701a25fef7a28f5bea2339a9e9b7c2b01488052
Marc Stevenson <marcsstevenson@hotmail.com>
2021-12-16 11:23:02 +1300

    Integrated Tailwind. Removed Bootstrap.

--- Aimly.Exchange.Frontend/src/components/Hero.tsx
+++ Aimly.Exchange.Frontend/src/components/Hero.tsx
@@ -1,16 +1,10 @@
-import React from "react";
+import React from 'react';
 
-import logo from "../assets/logo.svg";
+import logo from '../assets/logo.svg';
 
 const Hero = () => (
-  <div className="text-center hero my-5">
-    <img className="mb-3 app-logo" src={logo} alt="React logo" width="120" />
-    <h1 className="mb-4">React.js Sample Project</h1>
-
-    <p className="lead">
-      This is a sample application that demonstrates an authentication flow for
-      an SPA, using <a href="https://reactjs.org">React.js</a>
-    </p>
+  <div className="text-center">
+    <h1 className="font-bold underline first-letter:bg-red-400 first-letter:text-8xl text-5xl">Hello world!</h1>
   </div>
 );
 
