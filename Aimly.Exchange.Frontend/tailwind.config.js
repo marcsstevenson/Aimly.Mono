077fd3bbdb6170be8b43747db8cb51a6c5698c0a
Marc Stevenson <marcsstevenson@hotmail.com>
2022-02-10 10:44:16 +1300

    Adding standard buttons, custom colours to input and set list of used colours.

--- Aimly.Exchange.Frontend/tailwind.config.js
+++ Aimly.Exchange.Frontend/tailwind.config.js
@@ -1,9 +1,23 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   darkMode: 'class', // This means that dark mode is controlled by a class=dark on the base html element
   content: ["./src/**/*.{ts,tsx,js,jsx}", "./public/index.html"],
   colors: {},
   theme: {
-    extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      green: colors.green,
+      yellow: colors.yellow,
+      indigo: colors.indigo,
+      primary: colors.indigo,
+    },
+    extend: {
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),
