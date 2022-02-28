7aec835f55a6d5554ad29f6e72302301181dc681
Marc Stevenson <marcsstevenson@hotmail.com>
2022-03-01 10:54:17 +1300

    Handling delay in changing search term with the query getting updated variables

--- Aimly.Exchange.Frontend/src/components/market/MarketSearch.tsx
+++ Aimly.Exchange.Frontend/src/components/market/MarketSearch.tsx
@@ -8,6 +8,7 @@ import AppQuery, {
 } from '__generated__/marketSearchQuery.graphql';
 import { useQueryLoader } from 'react-relay';
 import { type ProfileTypeOption } from '__generated__/marketSearchQuery.graphql';
+import { PageHeader } from 'components/shared/PageHeader';
 
 interface Props {
   // CurrentPage: Pages;
@@ -16,61 +17,51 @@ interface Props {
 
 const MarketSearch = ({ CurrentProfileType }: Props) => {
   const [searchTerm, setSearchTerm] = useState<string | null>(null);
-  // let test = ProfileTypeOption.MENTOR;
-  const variables = useMemo<marketSearchQuery$variables>(() => {
+  const [queryRef, loadQuery] = useQueryLoader<marketSearchQuery>(AppQuery, null);
+
+  const buildVariables = useCallback((searchTerm: string, profileType: ProfileTypeOption) => {
     return {
       marketSearchCommand: {
         searchTerm: searchTerm,
-        profileType: CurrentProfileType,
+        profileType: profileType,
         currentPage: 3,
         pageSize: 3,
         orderByAscending: true,
         orderBy: 'Name',
       },
     };
-  }, [searchTerm, CurrentProfileType]);
-  const [queryRef, loadQuery] = useQueryLoader<marketSearchQuery>(AppQuery, null);
+  }, []);
 
   const handleSearchRequest = (searchTerm: string) => {
-    // Run search here
+    // Build the updated variables
+    var variables = buildVariables(searchTerm, CurrentProfileType);
+
     console.log('searchTerm: ', searchTerm);
+
+    // Remember the search term
     setSearchTerm(searchTerm);
-    refetch();
+
+    // Re-run the search
+    refetch(variables);
   };
 
-  const refetch = useCallback(() => {
-    // Load the query again using the same original variables.
-    // Calling loadQuery will update the value of queryRef.
-    // and this will update the usePreloadedQuery value for the MarketSearchResults
-    // component which will in-turn trigger that component to rerender.
-    loadQuery(variables);
-  }, [loadQuery, variables]);
+  const refetch = useCallback(
+    (variables: marketSearchQuery$variables) => {
+      // Load the query again using the same original variables.
+      // Calling loadQuery will update the value of queryRef.
+      // and this will update the usePreloadedQuery value for the MarketSearchResults
+      // component which will in-turn trigger that component to rerender.
+      loadQuery(variables);
+    },
+    [loadQuery]
+  );
 
   return (
     <div className="flex-1 pb-8">
-      {/* Page header */}
-      <div className="mb-8 bg-white shadow dark:bg-gray-700">
-        <div className="px-4 sm:px-6 lg:mx-auto lg:max-w-6xl lg:px-8">
-          <div className="py-6 md:flex md:items-center md:justify-between">
-            <div className="min-w-0 flex-1">
-              <div className="flex items-center">
-                <div>
-                  <div className="flex items-center">
-                    <h1 className="text-2xl font-bold leading-7 text-gray-900 dark:text-gray-50 sm:truncate sm:leading-9">
-                      Market
-                    </h1>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <PageHeader Title="Market" />
       <MarketHeader CurrentProfileType={CurrentProfileType} />
       <MarketSearchInput onChange={handleSearchRequest} />
-      <div className="mx-auto mt-8 max-w-6xl">
-        {searchTerm && queryRef && <MarketSearchResults queryRef={queryRef} />}
-      </div>
+      {searchTerm && queryRef && <MarketSearchResults queryRef={queryRef} />}
     </div>
   );
 };
