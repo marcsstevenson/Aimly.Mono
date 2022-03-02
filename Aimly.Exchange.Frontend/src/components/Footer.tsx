14fca58cba294d19c8e61280949f9287f16433f0
Marc Stevenson <marcsstevenson@hotmail.com>
2022-03-02 15:45:00 +1300

    Corrected footer border

--- Aimly.Exchange.Frontend/src/components/Footer.tsx
+++ Aimly.Exchange.Frontend/src/components/Footer.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const navigation = {
   main: [
@@ -60,32 +60,36 @@ const navigation = {
       ),
     },
   ],
-}
+};
 
 const Footer = () => (
-  <footer>
-    <hr/>
-      <div className="max-w-7xl mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px-8">
-        <nav className="-mx-5 -my-2 flex flex-wrap justify-center" aria-label="Footer">
-          {navigation.main.map((item) => (
-            <div key={item.name} className="px-5 py-2">
-              <Link to={{ pathname: item.href }} className="text-base text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-300">
-                {item.name}
-              </Link>
-            </div>
-          ))}
-        </nav>
-        <div className="mt-8 flex justify-center space-x-6">
-          {navigation.social.map((item) => (
-            <a key={item.name} href={item.href} className="text-gray-400 hover:text-gray-500">
-              <span className="sr-only">{item.name}</span>
-              <item.icon className="h-6 w-6" aria-hidden="true" />
-            </a>
-          ))}
-        </div>
-        <p className="mt-8 text-center text-base text-gray-400">&copy; 2022 Aimly. All rights reserved.</p>
+  <footer className="border-t border-gray-200 py-12 dark:border-gray-500">
+    <div className="mx-auto max-w-7xl overflow-hidden px-4 sm:px-6  lg:px-8">
+      <nav className="-mx-5 -my-2 flex flex-wrap justify-center" aria-label="Footer">
+        {navigation.main.map((item) => (
+          <div key={item.name} className="px-5 py-2">
+            <Link
+              to={{ pathname: item.href }}
+              className="text-base text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-300"
+            >
+              {item.name}
+            </Link>
+          </div>
+        ))}
+      </nav>
+      <div className="mt-8 flex justify-center space-x-6">
+        {navigation.social.map((item) => (
+          <a key={item.name} href={item.href} className="text-gray-400 hover:text-gray-500">
+            <span className="sr-only">{item.name}</span>
+            <item.icon className="h-6 w-6" aria-hidden="true" />
+          </a>
+        ))}
       </div>
-    </footer>
+      <p className="mt-8 text-center text-base text-gray-400">
+        &copy; 2022 Aimly. All rights reserved.
+      </p>
+    </div>
+  </footer>
 );
 
 // Aimly 2022
