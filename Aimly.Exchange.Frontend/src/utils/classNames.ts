6fe627b20e85433cd5187c3e71a0fad4151c69e9
Marc Stevenson <marcsstevenson@hotmail.com>
2022-03-07 10:46:36 +1300

    Trigger build

--- Aimly.Exchange.Frontend/src/utils/classNames.ts
+++ Aimly.Exchange.Frontend/src/utils/classNames.ts
@@ -1,4 +1,5 @@
-/// A simple utility to combine class names together
+// A simple utility to combine class names together
+
 export function classNames(...classes: string[]): string {
   return classes.filter(Boolean).join(' ');
 }
