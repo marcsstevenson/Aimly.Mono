588d7a7139a2f31abb8a716bab95cfa195e66bdb
Marc Stevenson <marcsstevenson@hotmail.com>
2022-05-17 08:09:26 +1200

    Re-adjustment of how profile items list

--- Aimly.Exchange.Frontend/src/components/profiles/MyProfiles.tsx
+++ Aimly.Exchange.Frontend/src/components/profiles/MyProfiles.tsx
@@ -101,9 +101,7 @@ const Profiles = () => {
         <h2 className="text-lg font-medium leading-6 text-gray-900 dark:text-gray-50">
           My Profiles
         </h2>
-        <div className="mt-2 grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3">
-          <MyProfilesList />
-        </div>
+        <MyProfilesList />
       </div>
     </div>
   );
