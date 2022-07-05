c70d1082e71224447e435b6503b27539e377064b
Marc Stevenson <marcsstevenson@hotmail.com>
2022-07-05 14:16:14 +1200

    Further fun with GA

--- Aimly.Exchange.Frontend/src/App.tsx
+++ Aimly.Exchange.Frontend/src/App.tsx
@@ -7,19 +7,19 @@ import { LoadingArea } from 'components/shared/LoadingArea';
 import { useAuth0, withAuthenticationRequired, User } from '@auth0/auth0-react';
 import PrivateShell from 'components/PrivateShell';
 import Loading from 'components/Loading';
-import { getConfig } from 'config';
-import ReactGA from 'react-ga';
+// import { getConfig } from 'config';
+// import ReactGA from 'react-ga';
 
-const config = getConfig();
+// const config = getConfig();
 
-// Initialise Google Analytics if we have a tracking Id
-if (config.analyticsTrackingId) {
-  console.log('Initialising Google Analytics', config.analyticsTrackingId);
-  ReactGA.initialize(config.analyticsTrackingId, {
-    debug: true,
-  });
-  ReactGA.pageview(window.location.pathname + window.location.search);
-}
+// // Initialise Google Analytics if we have a tracking Id
+// if (config.analyticsTrackingId) {
+//   console.log('Initialising Google Analytics', config.analyticsTrackingId);
+//   ReactGA.initialize(config.analyticsTrackingId, {
+//     debug: true,
+//   });
+//   ReactGA.pageview(window.location.pathname + window.location.search);
+// }
 
 const App = (): JSX.Element => {
   const { isLoading, error, user } = useAuth0<User>();
