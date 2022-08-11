76dbb296048879554b564053efca583fd0327725
Marc Stevenson <marcsstevenson@hotmail.com>
2022-08-11 20:58:22 +1200

    Added market search results count label

--- Aimly.Exchange.Frontend/src/components/market/MarketSearchResultsComponent.tsx
+++ Aimly.Exchange.Frontend/src/components/market/MarketSearchResultsComponent.tsx
@@ -20,6 +20,15 @@ export const MarketSearchResults = ({ queryRef }: Props) => {
       <div className="z-50 flex flex-col">
         <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
           <div className="inline-block min-w-full py-6 align-middle sm:px-6 lg:px-8">
+            {response.marketSearch?.results && response.marketSearch?.results?.length > 0 && (
+              <div className="ml-5 dark:ml-0 dark:mb-5">
+                {response.marketSearch?.results?.length} results
+                {response.marketSearch.totalResultCount > response.marketSearch.results.length && (
+                  <span> of {response.marketSearch.totalResultCount}</span>
+                )}
+              </div>
+            )}
+
             <div className="overflow-hidden sm:rounded-lg">
               <div className="ml-auto flex items-center space-x-5 bg-gray-50 px-6 py-3 dark:bg-gray-800"></div>
               {response.marketSearch?.results && response.marketSearch?.results?.length > 0 ? (
