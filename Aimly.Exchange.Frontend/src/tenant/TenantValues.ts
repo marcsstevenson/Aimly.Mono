6838a6f8cfbf229aabceba77b03bf1b22707e622
Marc Stevenson <marcsstevenson@hotmail.com>
2022-11-17 20:52:30 +1300

    Corrected social links for QL

--- Aimly.Exchange.Frontend/src/tenant/TenantValues.ts
+++ Aimly.Exchange.Frontend/src/tenant/TenantValues.ts
@@ -7,10 +7,10 @@ const CurrentTenant: TenantType = {
   primaryColour: 'slate',
   secondaryColour: 'sky',
   socialLinks: [
-    { type: 'Facebook', href: 'https://www.facebook.com/catallyze.movement' },
-    { type: 'Instagram', href: 'https://www.instagram.com/catallyze.movement/' },
-    { type: 'LinkedIn', href: 'https://www.linkedin.com/company/catallyze/mycompany/' },
-    { type: 'Twitter', href: 'https://twitter.com/catallyze' },
+    { type: 'Facebook', href: 'https://www.facebook.com/qualitymindglobal' },
+    { type: 'Instagram', href: 'https://www.instagram.com/qualitymindglobal/?hl=en' },
+    { type: 'LinkedIn', href: 'https://www.linkedin.com/company/quality-mind-global/' },
+    // { type: 'Twitter', href: 'https://twitter.com/catallyze' },
   ],
   companyOptions: {
     singularName: 'Company',
