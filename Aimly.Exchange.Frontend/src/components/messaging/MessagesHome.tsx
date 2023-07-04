cebcdbeceed0a0eaf1e973c14778e1c583181293
Marc Stevenson <marcsstevenson@hotmail.com>
2023-07-04 17:18:35 +1200

    Updated Sendbird to UIKit v3 and chat API v4

--- Aimly.Exchange.Frontend/src/components/messaging/MessagesHome.tsx
+++ Aimly.Exchange.Frontend/src/components/messaging/MessagesHome.tsx
@@ -1,71 +1,71 @@
-import React from 'react';
-
-const MessagesHome = () => {
-  return <></>;
-};
-
-// import React, { Suspense, useContext, useEffect } from 'react';
-// import 'sendbird-uikit/dist/index.css';
-// import { PrivateContext } from 'components/PrivateContext';
-// import { getConfig } from 'config';
-// // import CustomisedSendBirdApp from 'components/messaging/CustomisedSendBirdApp';
-// import UncustomisedSendBirdApp from 'components/messaging/UncustomisedSendBirdApp';
-// import { LoadingArea } from 'components/shared/LoadingArea';
-// import useLoginToMessagingMutation from 'useLoginToMessagingMutation';
-// import {
-//   UserLoginModelInput,
-//   useLoginToMessagingMutation$data,
-// } from '__generated__/useLoginToMessagingMutation.graphql';
+// import React from 'react';
 
 // const MessagesHome = () => {
-//   const { checkedInUser, userId, messagingAccessToken, loginToMessaging } =
-//     useContext(PrivateContext);
-//   // const [accessToken, setAccessToken] = useState<string | null>(messagingAccessToken);
-//   const config = getConfig();
-//   const LoginToMessaging = useLoginToMessagingMutation();
-
-//   // Run this once on mount
-//   useEffect(() => {
-//     if (checkedInUser?.publicId && checkedInUser?.fullName && !messagingAccessToken) {
-//       const model: UserLoginModelInput = {
-//         publicId: checkedInUser.publicId,
-//         fullName: checkedInUser.fullName ?? '',
-//         pictureUrl: checkedInUser.pictureUrl ?? '',
-//       };
-//       LoginToMessaging(userId ?? '', model, handleLoginCompleted);
-//     }
-//     // eslint-disable-next-line react-hooks/exhaustive-deps
-//   }, []);
-
-//   // This is called once the SetMentorProfile mutation has completed
-//   const handleLoginCompleted = (
-//     loginToMessagingModel: UserLoginModelInput,
-//     response: useLoginToMessagingMutation$data
-//   ): void => {
-//     if (response.loginToMessaging?.accessToken) {
-//       console.log(response.loginToMessaging.accessToken);
-//       // setAccessToken(response.loginToMessaging.accessToken);
-//       loginToMessaging(response.loginToMessaging.accessToken);
-//     }
-//   };
-
-//   return (
-//     <Suspense fallback={<LoadingArea title="Securing your messages..." fullHeight={false} />}>
-//       {checkedInUser?.publicId && messagingAccessToken && (
-//         <div className="h-90v">
-//           <UncustomisedSendBirdApp
-//             appId={config.sendBirdAppId}
-//             userId={checkedInUser.publicId}
-//             accessToken={messagingAccessToken}
-//           />
-//           {/* <CustomisedSendBirdApp appId={config.sendBirdAppId} userId={publicId} /> */}
-//         </div>
-//       )}
-//       {!messagingAccessToken && (
-//         <LoadingArea title="Securing your messages..." fullHeight={false} />
-//       )}
-//     </Suspense>
-//   );
+//   return <></>;
 // };
+
+import React, { Suspense, useContext, useEffect } from 'react';
+// import 'sendbird-uikit/dist/index.css';
+import { PrivateContext } from 'components/PrivateContext';
+import { getConfig } from 'config';
+// import CustomisedSendBirdApp from 'components/messaging/CustomisedSendBirdApp';
+import UncustomisedSendBirdApp from 'components/messaging/UncustomisedSendBirdApp';
+import { LoadingArea } from 'components/shared/LoadingArea';
+import useLoginToMessagingMutation from 'useLoginToMessagingMutation';
+import {
+  UserLoginModelInput,
+  useLoginToMessagingMutation$data,
+} from '__generated__/useLoginToMessagingMutation.graphql';
+
+const MessagesHome = () => {
+  const { checkedInUser, userId, messagingAccessToken, loginToMessaging } =
+    useContext(PrivateContext);
+  // const [accessToken, setAccessToken] = useState<string | null>(messagingAccessToken);
+  const config = getConfig();
+  const LoginToMessaging = useLoginToMessagingMutation();
+
+  // Run this once on mount
+  useEffect(() => {
+    if (checkedInUser?.publicId && checkedInUser?.fullName && !messagingAccessToken) {
+      const model: UserLoginModelInput = {
+        publicId: checkedInUser.publicId,
+        fullName: checkedInUser.fullName ?? '',
+        pictureUrl: checkedInUser.pictureUrl ?? '',
+      };
+      LoginToMessaging(userId ?? '', model, handleLoginCompleted);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  // This is called once the SetMentorProfile mutation has completed
+  const handleLoginCompleted = (
+    loginToMessagingModel: UserLoginModelInput,
+    response: useLoginToMessagingMutation$data
+  ): void => {
+    if (response.loginToMessaging?.accessToken) {
+      console.log(response.loginToMessaging.accessToken);
+      // setAccessToken(response.loginToMessaging.accessToken);
+      loginToMessaging(response.loginToMessaging.accessToken);
+    }
+  };
+
+  return (
+    <Suspense fallback={<LoadingArea title="Securing your messages..." fullHeight={false} />}>
+      {checkedInUser?.publicId && messagingAccessToken && (
+        <div className="h-90v">
+          <UncustomisedSendBirdApp
+            appId={config.sendBirdAppId}
+            userId={checkedInUser.publicId}
+            accessToken={messagingAccessToken}
+          />
+          {/* <CustomisedSendBirdApp appId={config.sendBirdAppId} userId={publicId} /> */}
+        </div>
+      )}
+      {!messagingAccessToken && (
+        <LoadingArea title="Securing your messages..." fullHeight={false} />
+      )}
+    </Suspense>
+  );
+};
 
 export default MessagesHome;
--- Aimly.Exchange.Frontend/src/components/messaging/UncustomisedSendBirdApp.tsx
+++ Aimly.Exchange.Frontend/src/components/messaging/UncustomisedSendBirdApp.tsx
@@ -1,47 +1,46 @@
-export {};
+import React, { useContext, useMemo } from 'react';
 
-// import React, { useContext, useMemo } from 'react';
-// import { App as SendbirdApp } from 'sendbird-uikit';
-// import 'sendbird-uikit/dist/index.css';
-// import { ThemeContext } from 'components/ThemeContext';
-// import { buildSendBirdColourSet } from 'components/messaging/SendBirdColourSet';
+import { ThemeContext } from 'components/ThemeContext';
+import { buildSendBirdColourSet } from 'components/messaging/SendBirdColourSet';
+import SendbirdApp from '@sendbird/uikit-react/App';
+import '@sendbird/uikit-react/dist/index.css';
 
-// interface SendBirdAppProps {
-//   appId: string;
-//   userId: string;
-//   accessToken: string;
-// }
+interface SendBirdAppProps {
+  appId: string;
+  userId: string;
+  accessToken: string;
+}
 
-// // The purpose of this component is to wrap the standard SendbirdApp with the
-// // required configuration such as appId, userId, theme, colour set and accessToken
-// const UncustomisedSendBirdApp = ({ appId, userId, accessToken }: SendBirdAppProps) => {
-//   const themeContext = useContext(ThemeContext);
+// The purpose of this component is to wrap the standard SendbirdApp with the
+// required configuration such as appId, userId, theme, colour set and accessToken
+const UncustomisedSendBirdApp = ({ appId, userId, accessToken }: SendBirdAppProps) => {
+  const themeContext = useContext(ThemeContext);
 
-//   const chatTheme = useMemo<'light' | 'dark'>(() => {
-//     if (themeContext?.isDark) {
-//       return 'dark';
-//     }
+  const chatTheme = useMemo<'light' | 'dark'>(() => {
+    if (themeContext?.isDark) {
+      return 'dark';
+    }
 
-//     return 'light';
-//   }, [themeContext?.isDark]);
+    return 'light';
+  }, [themeContext?.isDark]);
 
-//   const myColorSet = buildSendBirdColourSet();
+  const myColorSet = buildSendBirdColourSet();
 
-//   // Testing
-//   // appId = '60E4E56A-BE80-443A-815A-0FC074E2EEB9'; // Catallyze Exchange
-//   // userId = '1e783564-bb5a-45f9-bef0-2eb3b47e5234'; // Marc LinkedIn
+  // Testing
+  // appId = '60E4E56A-BE80-443A-815A-0FC074E2EEB9'; // Catallyze Exchange
+  // userId = '1e783564-bb5a-45f9-bef0-2eb3b47e5234'; // Marc LinkedIn
 
-//   return (
-//     <SendbirdApp
-//       appId={appId}
-//       userId={userId}
-//       accessToken={accessToken}
-//       theme={chatTheme}
-//       // This is a premium feature
-//       // showMagnifyingGlassIcon={false}
-//       colorSet={myColorSet}
-//     />
-//   );
-// };
+  return (
+    <SendbirdApp
+      appId={appId}
+      userId={userId}
+      accessToken={accessToken}
+      theme={chatTheme}
+      // This is a premium feature
+      // showMagnifyingGlassIcon={false}
+      colorSet={myColorSet}
+    />
+  );
+};
 
-// export default UncustomisedSendBirdApp;
+export default UncustomisedSendBirdApp;
