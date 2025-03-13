e3b92a2641ebfc292587fd97e06eb4bf64f5b15a
Marc Stevenson <marcsstevenson@hotmail.com>
2025-03-14 12:03:41 +1300

    Progress

--- Aimly.Exchange.Frontend/vite.config.mjs
+++ Aimly.Exchange.Frontend/vite.config.mjs
@@ -2,14 +2,39 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import legacy from '@vitejs/plugin-legacy'
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default defineConfig(() => {
+  const env = 'development';
+
   return {
     build: {
       sourcemap: true,
       outDir: 'build',
+      commonjsOptions: {
+        transformMixedEsModules: true,
+        include: [/node_modules/]
+      }
     },
-    plugins: [react(), tsconfigPaths()],
+    plugins: [
+      react({
+        babel: {
+          plugins: [
+            ['babel-plugin-relay', { eagerEsModules: true }]
+          ],
+          presets: ['@babel/preset-typescript']
+        }
+      }),
+      legacy({
+        targets: ['defaults', 'not IE 11'],
+        additionalLegacyPolyfills: ['regenerator-runtime/runtime']
+      }),
+      tsconfigPaths()
+    ],
     resolve: {
       alias: [
         {
@@ -19,8 +44,16 @@ export default defineConfig(() => {
       ]
     },
     define: {
-      'process.env': {},
-      global: {}
+      'process.env.NODE_ENV': JSON.stringify(env),
+      global: 'globalThis',
+    },
+    optimizeDeps: {
+      include: ['react-relay', 'relay-runtime'],
+      esbuildOptions: {
+        define: {
+          global: 'globalThis'
+        }
+      }
     }
   };
 });
